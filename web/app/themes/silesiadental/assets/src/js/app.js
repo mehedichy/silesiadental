f62f8cd14c8d343f05384e28e6da8c441020a0a6
itsumrat <itsumrat@gmail.com>
2016-11-19 11:59:34 +0600

    upload folder

--- web/app/themes/silesiadental/assets/src/js/app.js
+++ web/app/themes/silesiadental/assets/src/js/app.js
@@ -43,12 +43,12 @@ jQuery(document).ready(function($){
     // FAQs
     $('.collapse.in').prev('.panel-heading').addClass('active');
     $('#accordion, #bs-collapse')
-        .on('show.bs.collapse', function (a) {
-            $(a.target).prev('.panel-heading').addClass('active');
-        })
-        .on('hide.bs.collapse', function (a) {
-            $(a.target).prev('.panel-heading').removeClass('active');
-        });
+    .on('show.bs.collapse', function (a) {
+        $(a.target).prev('.panel-heading').addClass('active');
+    })
+    .on('hide.bs.collapse', function (a) {
+        $(a.target).prev('.panel-heading').removeClass('active');
+    });
 
 
 
@@ -57,7 +57,6 @@ jQuery(document).ready(function($){
       'resizeDuration': 200,
       'wrapAround': true
     });
-
             
 
 });
